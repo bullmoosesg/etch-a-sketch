181682e8d84cec4335976a2780732032cd6b0c1a
bullmoosesg <bullmoosesg@gmail.com>
2025-02-03 11:07:17 -0500

    Ongoing Build
    Darken functional, pixel add functional

--- main.js
+++ main.js
@@ -1,25 +1,78 @@
-let resButton = document.querySelector("#resize");
-let screen = document.querySelector('#frame');
-let rowCount = 16;
-//let square = document.createElement("div");
+const resButton = document.querySelector("#resize");
+const screen = document.querySelector('#screen');
+//const rowCount = 16;
+let square = document.createElement("div");
+square.setAttribute("class",'square');
 
 /* create function to change resolution
 create function to load squares
 create function to color pixels
 create function to add event listeners */
 
-let square = document.createElement("div");
-square.setAttribute("class",'square');
-
-screen.appendChild(square);
 
 
+function insertSquares(dim){
+    //clear the screen
+    while (screen.firstChild){
+        screen.removeChild(screen.firstChild);
+    }
+
+    let screenSize = 400 //make this dynamic later
+    
+
+    screen.appendChild(square);
+    
+
+    for (let i=1; i<dim**2;i++){
+        screen.appendChild(square.cloneNode());
+    }
+
+    let squareList = document.querySelectorAll(".square")
+
+    squareList.forEach((square)=>{
+        let squareSize = screenSize/dim;
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+    });
+
+};
 function darken(square){
-    let opacity = square.styles.opacity;
+    let opacity = square.style.opacity;
+    
 
     if (opacity < 1){
-        opacity += .1;
-    }
-}
+        square.style.opacity += 1;
+        console.log(`triggered, opacity changed`);
+    } else {
+    console.log(`triggered, opacity is at max `);}
+} 
+
+insertSquares(4);
+
+resButton.addEventListener("click",()=>{
+    let size = prompt("How many squares?")
+    //add code to validate its an integer and cap at 100
+    while (Number.isNaN(size) = false && size >= 100){
+        size = prompt("invalid, enter an integer between 1 and 100")
+    };
+    insertSquares(size)
+});
+/* 
+This isnt working to progressively darken, does not re-read opacity
+function readOpacity(square){
+    let opacity = square.style.opacity;
+    return opacity
+};
+
+function darken(square){
+    let opacity = readOpacity(square);
+    
+
+    if (opacity < 1){
+        square.style.opacity += .1;
+        console.log(`triggered, opacity is now ${opacity}`);
+    } else {
+    console.log(`triggered, opacity is at max ${opacity}`);}
+} */
 
 //square.addEventListener("mouseover",()=>{darken(square)});
